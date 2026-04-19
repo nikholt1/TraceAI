878c552b975c3c49e4951b87e48180f75a6dde86
nikholt1 <niklas1997@live.dk>
2026-04-19 09:16:55 +0200

    UserController IMPL

---
+++ bff/src/main/java/org/example/bff/controller/UserController.java
@@ -0,0 +1,38 @@
+package org.example.bff.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
+    public record UserResponse(String userName, List<String> role) {
+
+    }
+
+    @GetMapping
+    public UserResponse getUser(Authentication authentication) {
+        String subject = authentication.getName();
+        List<String> roles = authentication.getAuthorities().stream()
+                .filter(role -> role.getAuthority().startsWith("ROLE_"))
+                .map(role -> role.getAuthority().substring(5))
+                .toList();
+
+        if (roles.isEmpty()) {
+            roles = List.of("USER");
+        }
+        return new UserResponse(subject, roles);
+    }
+
+}
